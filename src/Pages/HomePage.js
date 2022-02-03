2b9b69202681484af13ae610e4cd05a11dca5f76
hicones <henriquealexandreaa@hotmail.com>
2022-02-03 10:57:00 -0300

    Mudança no texto de apresentação

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -14,8 +14,8 @@ function HomePage() {
             <div className="typography">
                 <h1>Olá, eu sou <span>Henrique Almeida</span></h1>
                 <p>
-                    Desenvolvedor Front-End Pleno, utilizando ReactJS
-                    Estudante de Análise e Desenvolvimento de Sistemas pela FIAP 
+                    Desenvolvedor Front-End Pleno utilizando ReactJS,
+                    Estudante de Análise e Desenvolvimento de Sistemas pela FIAP!
                 </p>
                 <div className="icons">
                     <a href="https://www.linkedin.com/in/henrique-almeida-079867169/" className="icon i-linkedin" target="blank">
